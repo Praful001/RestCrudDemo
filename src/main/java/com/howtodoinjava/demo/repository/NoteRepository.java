f08acac2fca5038db1e010ed73f7b46c0087e2ba
Praful001 <pitapure@gmail.com>
2020-03-14 19:58:32 +0530

    initial commit

---
+++ src/main/java/com/howtodoinjava/demo/repository/NoteRepository.java
@@ -0,0 +1,11 @@
+package com.howtodoinjava.demo.repository;
+
+
+import com.howtodoinjava.demo.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
